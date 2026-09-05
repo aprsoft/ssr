da15c544578b4cd02969e006b348a5fe8d895f35
aprsoft <r.rojas.ruiz@gmail.com>
2026-09-04 21:38:06 -0400

    update

--- routes/central.php
+++ routes/central.php
@@ -95,14 +95,15 @@ use Illuminate\Support\Facades\Route;
         Route::get('roles/create', [RoleController::class, 'create'])
             ->name('roles.create');
 
+        Route::get('roles/{role}/edit', [RoleController::class, 'edit'])
+            ->name('roles.edit');
+        
+        Route::put('roles/{role}', [RoleController::class, 'update'])
+            ->name('roles.update');
+        
         Route::get('roles/{role}', [RoleController::class, 'show'])
             ->name('roles.show');
         
-        Route::get('roles/{role}', [RoleController::class, 'edit'])
-            ->name('roles.edit');
-        
-        Route::get('roles/{role}', [RoleController::class, 'update'])
-            ->name('roles.update');
 
         
 
