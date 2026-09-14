c91520ef1a1aeaae2a695d23e08c4da6daedce07
aprsoft <r.rojas.ruiz@gmail.com>
2026-09-14 14:20:35 -0300

    update

--- database/factories/Central/ EmployeeFactory.php
+++ database/factories/Central/ EmployeeFactory.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Central\User>
  */
 class EmployeeFactory extends Factory
 {
