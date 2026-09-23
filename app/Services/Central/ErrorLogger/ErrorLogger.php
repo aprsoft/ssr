3a32168ae3add3bd75407de3a3679c2552cfd774
aprsoft <r.rojas.ruiz@gmail.com>
2026-09-22 23:30:29 -0300

    2026-09-22 23:30:29

--- app/Services/Central/ErrorLogger/ErrorLogger.php
+++ app/Services/Central/ErrorLogger/ErrorLogger.php
@@ -6,7 +6,7 @@ use App\Models\Central\ErrorLog;
 use Illuminate\Support\Facades\Log;
 use Throwable;
 
-class Error
+class ErrorLogger
 {
     /**
      * Registra una excepción en el log de Laravel y,
